026a5e4274796a2ca6032e513bc5ad36cb4f5c12
PraveenGandhi P <praveengandhi.p@gmail.com>
2020-06-16 18:18:30 +0530

    cut down to one node process

--- README.md
+++ README.md
@@ -3,19 +3,15 @@
 ### conventions achieved through @aurelia/plugin-conventions and @aurelia/plugin-gulp, 
 ### bundling through fuse-box 3.7.1
 
-## Tweaking bundling wtih two node processes 
-1. @aurelia/plugin-gulp
-    - [gulpfile.js](gulpfile.js) with gulp-replace
-2. fuse-box bundling and dev server
+
+## fuse-box bundling and dev server
     - [fuse.js](fuse.js)
 
 ## To install and run the app (in watch mode):
 
 ```bash
 npm install
-
-npm pre-proc (first terminal)
-npm start (second terminal)
+npm start
 ```
 
 [RegEx](https://regex101.com/r/vtMD1I/1)
--- fuse.js
+++ fuse.js
@@ -1,10 +1,16 @@
 const { FuseBox, CSSPlugin, HTMLPlugin, EnvPlugin, TerserPlugin, QuantumPlugin, WebIndexPlugin } = require("fuse-box");
 const { src, task } = require("fuse-box/sparky");
 
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const au2 = require('@aurelia/plugin-gulp').default;
+const chokidar = require('chokidar');
+const temp = 'temp_src';
+
 let run = (production) => {
     const fuse = FuseBox.init({
         target: "browser@es6",
-        homeDir: 'temp_src',
+        homeDir: temp,
         output: 'dist/$name.js',
         runAllMatchedPlugins: true,
         plugins: [
@@ -37,6 +43,19 @@ let run = (production) => {
     fuse.run();
 };
 
+function watchForChanges() {
+    chokidar.watch('src').on('all', (event, path) => {
+      console.log(event, path);
+      gulp.src(path)
+        .pipe(au2())
+        .pipe(replace(/(import.*\s+from\s+['"].*)(\.html)(["'];)/g, "$1$2.js$3"))
+        .pipe(gulp.dest(temp));
+    });
+}
+
 task('clean', async () => await src('dist/*').clean('dist').exec());
-task("dev",     ['clean'], () => run(false));
+task("dev",     ['clean'], () => {
+    watchForChanges();
+    run(false);
+});
 task("prod",    ['clean'], () => run(true)); 
